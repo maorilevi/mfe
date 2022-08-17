afcc101ca9803bff929d4ca5cea6a04faaf714c7
maorl <maorl@consist.co.il>
2022-08-17 11:21:39 +0300

    update container app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import MarketingApp from "./component/MarketingApp";
 
 export default () => {
-    return <div>
-        <hr/>
-        <MarketingApp />
-    </div>
+    return (
+        <div>
+            <hr/>
+            <MarketingApp />
+        </div>)
 }

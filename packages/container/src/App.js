148524e1cb7b3f4e8978f9ead2597dbf503be88e
maorl <maorl@consist.co.il>
2022-08-18 06:51:23 +0300

    finish feature number 1

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,6 +4,7 @@ import MarketingApp from "./component/MarketingApp";
 export default () => {
     return (
         <div>
+            <h1>Hi there!!!</h1>
             <hr/>
             <MarketingApp />
         </div>

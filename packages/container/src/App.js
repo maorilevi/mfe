2fa89f36e0df81cb0416ab5044706d0230c35030
maorl <maorl@consist.co.il>
2022-08-18 07:46:29 +0300

    scope styles

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import MarketingApp from "./component/MarketingApp";
 import Header from './component/Header';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+});
 export default () => {
     return (
-        <BrowserRouter>
-            <div>
-                <Header/>
-                <MarketingApp/>
-            </div>
-        </BrowserRouter>
+        <div>
+            <StylesProvider generateClassName={generateClassName}>
+                <BrowserRouter>
+                    <div>
+                        <Header/>
+                        <MarketingApp/>
+                    </div>
+                </BrowserRouter>
+            </StylesProvider>
+        </div>
     );
 }

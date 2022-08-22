3b0ac2207097f13b068e89d663488b63a40ba12b
maorl <maorl@consist.co.il>
2022-08-22 06:02:06 +0300

    added dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,40 +1,49 @@
-import React, {lazy, Suspense, useState} from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import React, {lazy, Suspense, useEffect, useState} from 'react';
+import {Router, Route, Switch, Redirect} from 'react-router-dom';
 import {createGenerateClassName, StylesProvider} from '@material-ui/core/styles';
-
-// import MarketingApp from "./component/MarketingApp";
-// import AuthApp from './component/AuthApp';
 import Progress from "./component/Progress";
 import Header from './component/Header';
+import {createBrowserHistory} from 'history';
+
 
 const MarketingLazy = lazy(() => import('./component/MarketingApp'));
 const AuthLazy = lazy(() => import('./component/AuthApp'));
+const DashboardLazy = lazy(() => import('./component/DashboardApp'));
+
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co'
 });
+const history = createBrowserHistory();
 export default () => {
     const [isSignedIn, setIsSignedIn] = useState(false);
+
+    useEffect(() => {
+        if (isSignedIn) history.push('/dashboard');
+    }, [isSignedIn]);
     const signInFn = () => {
         setIsSignedIn(true);
-        console.log('!!!!');
     }
     return (
         <div>
-            <StylesProvider generateClassName={generateClassName}>
-                <BrowserRouter>
+            <Router history={history}>
+                <StylesProvider generateClassName={generateClassName}>
                     <div>
                         <Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn}/>
                         <Suspense fallback={<Progress/>}>
                             <Switch>
-                                <Route path="/auth" >
+                                <Route path="/auth">
                                     <AuthLazy onSignIn={signInFn.bind(this)}/>
+                                </Route>
+                                <Route path="/dashboard">
+                                    {!isSignedIn && <Redirect to="/" />}
+                                    <DashboardLazy/>
                                 </Route>
                                 <Route path="/" component={MarketingLazy}/>
                             </Switch>
                         </Suspense>
                     </div>
-                </BrowserRouter>
-            </StylesProvider>
+                </StylesProvider>
+            </Router>
         </div>
     );
 }
--- packages/dashboard/config/webpack.dev.js
+++ packages/dashboard/config/webpack.dev.js
@@ -1,9 +1,10 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
-const port = 8080;
+const port = 8083;
 const devConfig = {
     mode: 'development',
     output: {
@@ -12,19 +13,24 @@ const devConfig = {
     devServer: {
         port: port,
         historyApiFallback: {
-            index: '/index.html'
+            index: 'index.html'
         },
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        }
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'container',
-            remotes: {
-                marketing: 'marketing@http://localhost:8081/remoteEntry.js',
-                auth: 'auth@http://localhost:8082/remoteEntry.js',
-                dashboard: 'dashboard@http://localhost:8083/remoteEntry.js',
+            name: 'dashboard',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './DashboardApp': './src/bootstrap'
             },
             shared: packageJson.dependencies,
-        })
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
     ]
 };
 
